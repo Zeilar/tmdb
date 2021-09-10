321e8ccca63f51cb11e9ed7f7734bf74b316691d
Philip <philip_angelin@hotmail.com>
2021-09-10 22:40:26 +0200

    Added local storage hook

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,3 +1,15 @@
-export function useLocalStorage() {
-	//
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string) {
+	const [data, setData] = useState<T>(() => {
+		const data = localStorage.getItem(key);
+		if (data === null) return data;
+		return JSON.parse(data);
+	});
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(data));
+	}, [data, key]);
+
+	return [data, setData] as const;
 }
