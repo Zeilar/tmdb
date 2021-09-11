027a8e457e47843a47410eba86d7c6a4c8dca5b0
Philip <philip_angelin@hotmail.com>
2021-09-11 18:07:40 +0200

    Fixed image url path
    Can't believe it actually worked with the extra slash still

--- src/services/images.ts
+++ src/services/images.ts
@@ -1,5 +1,5 @@
 const BASE_URL = "https://image.tmdb.org/t/p";
 
 export function getImageUrl(path: string, size: string = "w200") {
-	return `${BASE_URL}/${size}/${path}`;
+	return `${BASE_URL}/${size}${path}`;
 }
