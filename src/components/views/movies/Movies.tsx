0468f13f5e7fa1b218f7a215a3686b70b327f887
Philip <philip_angelin@hotmail.com>
2021-09-16 16:07:20 +0200

    Removed fetch first 2 pages gimmick, screw it

--- src/components/views/movies/Movies.tsx
+++ src/components/views/movies/Movies.tsx
@@ -1,5 +1,5 @@
 import { Flex, Heading } from "@chakra-ui/react";
-import { useRef, useEffect } from "react";
+import { useEffect } from "react";
 import { useInfiniteQuery } from "react-query";
 import { flattenMoviesQuery, getNextPage } from "../../../helpers";
 import { useScrollEvent } from "../../../hooks";
@@ -27,20 +27,6 @@ export function Movies({ queryID, callback, params }: Props) {
 	}
 
 	useScrollEvent(nextPage);
-
-	const fetchNextOnMount = useRef(true);
-
-	useEffect(() => {
-		// Get first 2 pages on mount to make the grid more full to start with
-		if (!fetchNextOnMount.current || !data?.pages) {
-			return;
-		}
-		const nextPage = getNextPage(data.pages[data.pages.length - 1]);
-		if (nextPage) {
-			fetchNextPage();
-			fetchNextOnMount.current = false;
-		}
-	}, [fetchNextPage, data]);
 
 	if (isError) {
 		return <Heading>Something went wrong!</Heading>;
