a9f472101bd929ba88d742c315d3b2b7d9443176
Philip <philip_angelin@hotmail.com>
2021-09-18 19:12:56 +0200

    Added recently viewed movies to home

--- src/components/views/home.tsx
+++ src/components/views/home.tsx
@@ -1,5 +1,6 @@
 import { Flex } from "@chakra-ui/react";
 import { useQuery } from "react-query";
+import { useLastVisitedMovies } from "../../hooks";
 import { getLatestMovies, getPopularMovies, getTopMovies } from "../../services";
 import { MovieGallery } from "../movie";
 
@@ -7,6 +8,7 @@ export function Home() {
 	const latestMovieQuery = useQuery(["movies-latest", 1], () => getLatestMovies());
 	const popularMoviesQuery = useQuery(["movies-popular", 1], () => getPopularMovies());
 	const topRatedMoviesQuery = useQuery(["movies-top", 1], () => getTopMovies());
+	const lastVisitedMovies = useLastVisitedMovies();
 
 	return (
 		<Flex flexDirection="column" gridGap="5rem">
@@ -28,6 +30,9 @@ export function Home() {
 				loading={topRatedMoviesQuery.isLoading}
 				movies={topRatedMoviesQuery.data?.results ?? []}
 			/>
+			{lastVisitedMovies.data && (
+				<MovieGallery header="Recently viewed movies" movies={lastVisitedMovies.data} />
+			)}
 		</Flex>
 	);
 }
